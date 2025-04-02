525b98ff0a30e6909e180e547bb047d8c0c72769
Tudor Morar <tudor.morar@elrond.com>
2025-04-02 16:02:10 +0300

    Passkey cross platform refactors (#18)
    * Added safeWindow
    
    * Minor refactoring
    
    * Changed axios version
    
    * Update CHANGELOG
    
    * ds store
    
    * Ignore ds store
    
    * Ignore coverage
    
    * Move to yarn
    
    * Changelog
    
    * git cache cleared

--- src/constants.ts
+++ src/constants.ts
@@ -2,3 +2,5 @@ export const PASSKEY_SERVICE_URL = 'https://testnet-extras-api.multiversx.com';
 export const PASSKEY_CHALLENGE_ENDPOINT = '/passkeys/challenge';
 export const PASSKEY_AUTHENTICATE_ENDPOINT = '/passkeys/authentication/verify';
 export const PASSKEY_REGISTER_ENDPOINT = '/passkeys/register/verify';
+
+export const safeWindow = typeof window !== 'undefined' ? window : undefined;
